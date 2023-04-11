c2fb89f0883af161ce8af6ace270e33e226e79b6
Peter-McClelland <pm926048@wcupa.edu>
2023-04-11 19:47:59 -0400

    Pollished the Web UI, made a clear chat option, need to add Rasa tho and then make it look cleaner

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import './index.css';
 function App() {
   const [messages, setMessages] = useState([]);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = (event) => {                 /* to clear the chat type /clear*/
     event.preventDefault();
     const input = event.target.elements.message.value;
-    setMessages([...messages, input]);
+    if (input.toLowerCase() === "/clear") {
+      setMessages([]);
+    } else {
+      setMessages([...messages, input]);
+    }
     event.target.elements.message.value = '';
   };
+
   return (
     <div className="App">
       <header class="App-header">
-      <div class="chatbot-container">
+        <div class="chatbot-container">
           <div class="chatbot-messages">
             {messages.map((message, index) => (
               <div class="message" key={index}>
@@ -21,10 +26,10 @@ function App() {
               </div>
             ))}
           </div>
-        <form class="form" onSubmit={handleSubmit}>
-        <input class="text" type="text" name="message"></input>
-        <input type="submit" class="submit" value="Send"></input>
-        </form>
+          <form class="form" onSubmit={handleSubmit}>
+          <input class="text" type="text" name="message" placeholder="Enter your question about a WCU class or Professor. Enter &quot;/clear&quot; to clear the chat."></input>
+          <input type="submit" class="submit" value="Send"></input>
+          </form>
         </div>
       </header>
     </div>
