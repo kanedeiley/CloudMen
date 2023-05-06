e8ca1af2b8a7d37614cc71e0b700988ad49808a5
CalvinMMiner <89927855+CalvinMMiner@users.noreply.github.com>
2023-05-06 19:48:10 -0400

    Knowledge Base

--- actions/actions.py
+++ actions/actions.py
@@ -4,38 +4,11 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import mysql.connector
 
-class ActionHelloWorld(Action):
-     def name(self) -> Text:
-         return "action_hello_world"
+from rasa_sdk.knowledge_base.storage import InMemoryKnowledgeBase
+from rasa_sdk.knowledge_base.actions import ActionQueryKnowledgeBase
 
-     def run(self, dispatcher: CollectingDispatcher,
-             tracker: Tracker,
-             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-         dispatcher.utter_message(text="Hello World!")
-
-         return []
-         
-class action_teacher_Name(Action):
-     def name(self) -> Text:
-         return "action_hello_world"
-
-     def run(self, dispatcher: CollectingDispatcher,
-             tracker: Tracker,
-             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-         dispatcher.utter_message(text="teacher Name!")
-
-         return []
-         
-class action_teacher_department(Action):
-     def name(self) -> Text:
-         return "action_hello_world"
-
-     def run(self, dispatcher: CollectingDispatcher,
-             tracker: Tracker,
-             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-         dispatcher.utter_message(text="teacher!")
-
-         return []
+class action_query_knowledge_base(ActionQueryKnowledgeBase):
+    def __init__(self):
+        knowledge_base = InMemoryKnowledgeBase("data.json")
+        super().__init__(knowledge_base)
+                 
