83cfb213fc632330339b8f0352bccfbe1a4a8ac9
CalvinMMiner <89927855+CalvinMMiner@users.noreply.github.com>
2023-05-08 18:31:26 -0400

    fingers crossed

--- actions/actions.py
+++ actions/actions.py
@@ -2,12 +2,161 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.knowledge_base.storage import InMemoryKnowledgeBase
-from rasa_sdk.knowledge_base.actions import ActionQueryKnowledgeBase
+#from rasa_sdk.knowledge_base.storage import InMemoryKnowledgeBase
+#from rasa_sdk.knowledge_base.actions import ActionQueryKnowledgeBase
 
-class action_query_knowledge_base(ActionQueryKnowledgeBase):
-    def __init__(self):
-        knowledge_base = InMemoryKnowledgeBase("data.json")
-
-        super().__init__(knowledge_base)
+#class action_query_knowledge_base(ActionQueryKnowledgeBase):
+#    def __init__(self):
+#        knowledge_base = InMemoryKnowledgeBase("data.json")
+#        super().__init__(knowledge_base)
                  
+import mysql.connector
+    
+class action_teacher_tdept(Action):
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="that teacher works in the:")
+
+        mydb = mysql.connector.connect(host="mysqldb ", user="root", password="basicPassword", database="profs ")
+
+        teacher = tracker.get_slot("tSid")
+        cursor = mydb.cursor()
+        query = "SELECT tdept, FROM events WHERE name=%s"
+        cursor.execute(query, (teacher))
+        for x in cursor:
+            dispatcher.utter_message(text=str(x) + "department") 
+
+        cursor.close()
+        mydb.close()
+
+        return []
+    
+class action_teacher_tNumRatings(Action):
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="that teacher has:")
+
+        mydb = mysql.connector.connect(host="mysqldb ", user="root", password="basicPassword", database="profs ")
+
+        teacher = tracker.get_slot("tSid")
+        cursor = mydb.cursor()
+        query = "SELECT tNumRatings, FROM events WHERE name=%s"
+        cursor.execute(query, (teacher))
+        for x in cursor:
+            dispatcher.utter_message(text=str(x) + "Ratings(")
+
+        cursor.close()
+        mydb.close()
+
+        return []
+
+class action_teacher_Quality(Action):
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        dispatcher.utter_message(text="that teacher has a listed quality of:")
+
+        mydb = mysql.connector.connect(host="mysqldb ", user="root", password="basicPassword", database="profs ")
+
+        teacher = tracker.get_slot("tSid")
+        cursor = mydb.cursor()
+        query = "SELECT Quality, FROM events WHERE name=%s"
+        cursor.execute(query, (teacher))
+        for x in cursor:
+            dispatcher.utter_message(text=str(x))
+
+        cursor.close()
+        mydb.close()
+
+        return []
+    
+class action_teacher_Difficulty(Action):
+
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        dispatcher.utter_message(text="that teacher has a average difficulty of:")
+
+        mydb = mysql.connector.connect(host="mysqldb ", user="root", password="basicPassword", database="profs ")
+
+        teacher = tracker.get_slot("tSid")
+        cursor = mydb.cursor()
+        query = "SELECT Difficulty, FROM events WHERE name=%s"
+        cursor.execute(query, (teacher))
+        for x in cursor:
+            dispatcher.utter_message(text=str(x))
+
+        cursor.close()
+        mydb.close()
+
+        return []
+
+class action_teacher_WTA(Action):
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="the students that would take that class again come out to:")
+
+        mydb = mysql.connector.connect(host="mysqldb ", user="root", password="basicPassword", database="profs ")
+
+        teacher = tracker.get_slot("tSid")
+        cursor = mydb.cursor()
+        query = "SELECT WTA, FROM events WHERE name=%s"
+        cursor.execute(query, (teacher))
+        for x in cursor:
+            dispatcher.utter_message(text=str(x))
+
+        cursor.close()
+        mydb.close()
+
+        return []
+    
+class action_teacher_WTA(Action):
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="the students that would take that class again come out to:")
+
+        teacher = tracker.get_slot("tSid")
+        cursor = mydb.cursor()
+        query = "SELECT WTA, FROM events WHERE name=%s"
+        cursor.execute(query, (teacher))
+        for x in cursor:
+            dispatcher.utter_message(text=str(x))
+
+        cursor.close()
+        mydb.close()
+
+        return []
+    
+class action_teacher_Name(Action):
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        teacher = tracker.get_slot("tSid")
+        cursor = mydb.cursor()
+        query = "SELECT tSid, FROM events WHERE name=%s"
+        cursor.execute(query, (teacher))
+        for x in cursor:
+            dispatcher.utter_message(text=str(x) + " is a teacher at WCU")
+
+        cursor.close()
+        mydb.close()
+
+        return []
